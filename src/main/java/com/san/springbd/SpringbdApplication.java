1967a67833f3d5687377108901cef78f2d176a7f
ksan41 <aquam41@gmail.com>
2020-09-18 17:43:38 +0900

    Init project

---
+++ src/main/java/com/san/springbd/SpringbdApplication.java
@@ -0,0 +1,13 @@
+package com.san.springbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/san/springbd/SpringbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.springbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
