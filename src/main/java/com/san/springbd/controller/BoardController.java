57feb8739964d2d4c992c75b0bc90b8463275429
ksan41 <aquam41@gmail.com>
2020-09-22 01:23:43 +0900

    Update BoardController.java
    게시글 상세조회 요청 메서드 추가

--- src/main/java/com/san/springbd/controller/BoardController.java
+++ src/main/java/com/san/springbd/controller/BoardController.java
@@ -1,12 +1,16 @@
 package com.san.springbd.controller;
 
+import com.san.springbd.domain.Board;
 import com.san.springbd.domain.member.Member;
+import com.san.springbd.repository.BoardRepository;
 import com.san.springbd.service.BoardService;
 import com.san.springbd.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,7 +19,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BoardController {
 
     private final BoardService boardService;
+    private final BoardRepository boardRepository;
     private final MemberService memberService;
+
+    /**
+     * 게시글 상세조회 요청
+     */
+    @Transactional
+    @GetMapping("/user/posts/{boardId}")
+    public String getOnePost(@PathVariable("boardId") Long boardId, Model model){
+        Board board = boardService.findById(boardId);
+        board.addCount();
+        model.addAttribute("board",board);
+
+        return "boardDetail";
+    }
 
     /**
      * 글 작성페이지 이동
