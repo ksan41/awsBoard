c4a728c3fb7bb14c00bebc2dac654bc02ffb7e57
ksan41 <aquam41@gmail.com>
2020-09-21 23:34:00 +0900

    Update HomeController.java
    홈 요청시 전체 게시글 불러오도록 요청

--- src/main/java/com/san/springbd/controller/HomeController.java
+++ src/main/java/com/san/springbd/controller/HomeController.java
@@ -1,16 +1,30 @@
 package com.san.springbd.controller;
 
+import com.san.springbd.domain.Board;
+import com.san.springbd.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final BoardService boardService;
+
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
        log.info("home controller");
+
+       List<Board> boards = boardService.findBoards();
+
+       model.addAttribute("boards",boards);
+
        return "home";
     }
 }
