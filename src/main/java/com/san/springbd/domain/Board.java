a18d211c39ff7cb91a56adac4e0e1351144ed564
ksan41 <aquam41@gmail.com>
2020-09-21 23:32:52 +0900

    Update Board.java

--- src/main/java/com/san/springbd/domain/Board.java
+++ src/main/java/com/san/springbd/domain/Board.java
@@ -25,6 +25,23 @@ public class Board {
 
     private LocalDateTime createDate;
 
+    private LocalDateTime modifyDate;
+
+    private int count; // 조회수
+
     @Enumerated(EnumType.STRING)
     private BoardStatus status; // 게시글 상태(VISIBLE, DELETE)
+
+    // == 생성 메서드==//
+
+    public static Board createBoard(Member member,String title,String content){
+        Board board=new Board();
+        board.member=member;
+        board.title=title;
+        board.content=content;
+        board.status=BoardStatus.VISIBLE;
+        board.createDate=LocalDateTime.now();
+        board.count=0;
+        return board;
+    }
 }
