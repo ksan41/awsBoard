56adf2b7780865ad85b86ce17c503068640750aa
ksan41 <aquam41@gmail.com>
2020-09-21 23:32:48 +0900

    Create BoardRepository.java

---
+++ src/main/java/com/san/springbd/repository/BoardRepository.java
@@ -0,0 +1,30 @@
+package com.san.springbd.repository;
+
+import com.san.springbd.domain.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 게시글 등록
+     */
+    public void save(Board board){
+        em.persist(board);
+    }
+
+    /**
+     * 전체 게시글 조회
+     */
+    public List<Board> findAll(){
+        return em.createQuery("select b from Board b order by b.createDate desc",Board.class)
+                .getResultList();
+    }
+}
