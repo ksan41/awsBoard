d463dd5a9a77cad374f88bd8ecde1f923f337194
ksan41 <aquam41@gmail.com>
2020-09-22 20:53:32 +0900

    Update BoardController.java
    게시글 상세 요청 메서드 수정
    게시글 삭제 요청 메서드 추가

--- src/main/java/com/san/springbd/controller/BoardController.java
+++ src/main/java/com/san/springbd/controller/BoardController.java
@@ -25,11 +25,9 @@ public class BoardController {
     /**
      * 게시글 상세조회 요청
      */
-    @Transactional
     @GetMapping("/user/posts/{boardId}")
     public String getOnePost(@PathVariable("boardId") Long boardId, Model model){
-        Board board = boardService.findById(boardId);
-        board.addCount();
+        Board board = boardService.selectBoard(boardId);
         model.addAttribute("board",board);
 
         return "boardDetail";
@@ -51,6 +49,34 @@ public class BoardController {
                              @RequestParam("title") String title,
                              @RequestParam("content") String content){
         boardService.createPost(loginId,title,content);
+        return "redirect:/";
+    }
+
+    /**
+     * 글 수정페이지 이동
+     */
+    @GetMapping("/user/posts/{boardId}/update")
+    public String updateForm(@PathVariable("boardId") Long boardId, Model model){
+        Board board = boardService.findById(boardId);
+        model.addAttribute("board",board);
+        return "boardUpdateForm";
+    }
+
+//    /**
+//     * 글 수정
+//     */
+//    @PostMapping("/user/posts/update")
+//    public String update(){
+//
+//    }
+
+    /**
+     * 글 삭제
+     */
+    @PostMapping("/user/posts/{boardId}/delete")
+    public String delete(@PathVariable("boardId") Long boardId){
+        System.out.println("게시글 번호: "+boardId);
+        boardService.delete(boardId);
         return "redirect:/";
     }
 }
