e07dcc2dfc1ca9d950d48f8ef2d3de31abdc919a
ksan41 <aquam41@gmail.com>
2020-09-24 02:23:14 +0900

    Update Reply.java
    Builder추가

--- src/main/java/com/san/springbd/domain/Reply.java
+++ src/main/java/com/san/springbd/domain/Reply.java
@@ -1,13 +1,18 @@
 package com.san.springbd.domain;
 
 import com.san.springbd.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="reply")
 public class Reply {
 
     @Id
@@ -17,17 +22,28 @@ public class Reply {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
-    private Member member;
+    private Long memberId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="board_id")
-    private Board refBoard;
+    private Long refBoard;
 
     private String content;
 
     private LocalDateTime createDate;
 
+    private LocalDateTime modifyDate;
+
     @Enumerated(EnumType.STRING)
     private BoardStatus status;
 
+    @Builder
+    public Reply(Long memberId, Long refBoard, String content,LocalDateTime createDate,
+                 BoardStatus status){
+        this.memberId=memberId;
+        this.refBoard=refBoard;
+        this.content=content;
+        this.createDate=LocalDateTime.now();
+        this.status=BoardStatus.VISIBLE;
+    }
 }
