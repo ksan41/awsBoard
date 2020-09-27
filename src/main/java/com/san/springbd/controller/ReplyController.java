5c1d7fbb5a8941a424d3aaa9a7c263faf35b4a12
ksan41 <aquam41@gmail.com>
2020-09-27 22:34:10 +0900

    Update ReplyController.java
    Gson 사용하지 않도록 변경

--- src/main/java/com/san/springbd/controller/ReplyController.java
+++ src/main/java/com/san/springbd/controller/ReplyController.java
@@ -2,22 +2,14 @@ package com.san.springbd.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.san.springbd.domain.Board;
-import com.san.springbd.domain.Reply;
 import com.san.springbd.repository.BoardRepository;
 import com.san.springbd.repository.ReplyQueryRepository;
-import com.san.springbd.service.BoardService;
 import com.san.springbd.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,10 +25,16 @@ public class ReplyController {
     @GetMapping(value = "/user/reply/{boardId}",produces = "application/json; charset=utf-8")
     public @ResponseBody String getAllReplies(@PathVariable("boardId") Long boardId){
         Board board = boardRepository.findById(boardId);
+        ObjectMapper mapper = new ObjectMapper();
 
         if(board.getReplyCount()>0){
-                String jsonData= new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create().toJson(replyQueryRepository.findByRefBoard(boardId));
-                return jsonData;
+            String jsonData= null;
+            try {
+                jsonData = mapper.writeValueAsString(replyQueryRepository.findByRefBoard(boardId));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            return jsonData;
 
         }
         return "";
