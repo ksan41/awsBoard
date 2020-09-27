7a77713d6411f656951cb86a911d002380c9426c
ksan41 <aquam41@gmail.com>
2020-09-27 21:35:13 +0900

    Create ReplyRepository.java

---
+++ src/main/java/com/san/springbd/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.san.springbd.repository;
+
+import com.san.springbd.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ReplyRepository extends JpaRepository<Reply,Long> {
+
+}
