c5d30e47e110284913510dd9d4256fb155388bee
ksan41 <aquam41@gmail.com>
2020-09-27 21:33:03 +0900

    Update BoardService.java
    테스트용 출력문 등 제거

--- src/main/java/com/san/springbd/service/BoardService.java
+++ src/main/java/com/san/springbd/service/BoardService.java
@@ -1,7 +1,6 @@
 package com.san.springbd.service;
 
 import com.san.springbd.domain.Board;
-import com.san.springbd.domain.member.CustomUserDetails;
 import com.san.springbd.domain.member.Member;
 import com.san.springbd.repository.BoardRepository;
 import com.san.springbd.repository.MemberRepository;
@@ -26,7 +25,6 @@ public class BoardService {
     @Transactional
     public Long createPost(String loginId,String title,String content){
         // 유저 정보 조회
-        System.out.println("아이디: "+loginId);
         Optional<Member> userEntityWrapper = memberRepository.findByLoginId(loginId);
         Member member = userEntityWrapper.get();
 
