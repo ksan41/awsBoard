611f39ddfe89c99a5b274f120a821a961823ec54
ksan41 <aquam41@gmail.com>
2020-09-27 22:32:52 +0900

    Update Reply.java

--- src/main/java/com/san/springbd/domain/Reply.java
+++ src/main/java/com/san/springbd/domain/Reply.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.san.springbd.domain.member.Member;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
