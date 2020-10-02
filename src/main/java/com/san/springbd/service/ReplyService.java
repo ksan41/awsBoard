0a811917e658a2eece0d09c8ec261e105f290957
ksan41 <aquam41@gmail.com>
2020-10-02 16:38:59 +0900

    Update ReplyService, boardDetail.html
    댓글작성 반환값 수정
    (int -> Long ReplyId)

--- src/main/java/com/san/springbd/service/ReplyService.java
+++ src/main/java/com/san/springbd/service/ReplyService.java
@@ -25,8 +25,7 @@ public class ReplyService {
      * 댓글 작성
      */
     @Transactional
-    public int createReply(Long boardId, String loginId,String content){
-        int result = 0;
+    public Long createReply(Long boardId, String loginId,String content){
         // 유저 정보 조회
         Optional<Member> userEntityWrapper = memberRepository.findByLoginId(loginId);
         Member member = userEntityWrapper.get();
@@ -34,13 +33,11 @@ public class ReplyService {
         Board board =  boardRepository.findById(boardId);
 
         Reply reply = Reply.createReply(member.getLoginId(),member.getNickname(),boardId,content);
-        replyRepository.save(reply);
+        Long savedId = replyRepository.save(reply).getId();
 
-        if(reply!=null){
-         result=1;
-         board.addReplyCount();
-        }
-        return result;
+        board.addReplyCount();
+
+        return savedId;
     }
 
     /**
