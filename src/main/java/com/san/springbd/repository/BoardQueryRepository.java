691cf0302654c64d3064cb1ebd1534168614efa1
ksan41 <aquam41@gmail.com>
2020-10-03 15:56:10 +0900

    Update BoardQueryRepository.java
    findAll 메서드 Page객체로 반환하도록 변경

--- src/main/java/com/san/springbd/repository/BoardQueryRepository.java
+++ src/main/java/com/san/springbd/repository/BoardQueryRepository.java
@@ -1,12 +1,15 @@
 package com.san.springbd.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.san.springbd.domain.Board;
 import com.san.springbd.domain.BoardStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 import static com.san.springbd.domain.QBoard.board;
 
@@ -15,10 +18,14 @@ import static com.san.springbd.domain.QBoard.board;
 public class BoardQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public List<Board> findAll(){
-        return queryFactory.selectFrom(board)
+    public Page<Board> findAll(PageRequest pageRequest){
+        QueryResults<Board> result =
+                 queryFactory.selectFrom(board)
                 .where(board.status.eq(BoardStatus.VISIBLE))
                 .orderBy(board.createDate.desc())
-                .fetch();
+                .offset(pageRequest.getOffset())
+                .limit(pageRequest.getPageSize())
+                .fetchResults();
+        return new PageImpl<>(result.getResults(),pageRequest,result.getTotal());
     }
 }
