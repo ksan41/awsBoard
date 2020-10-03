11b624cc296bbd78bcc5712817c646b60114bfda
ksan41 <aquam41@gmail.com>
2020-10-03 15:57:39 +0900

    Update HomeController.java
    홈 화면 요청시 페이지 값도 같이 요청,
    불러온 게시글을 Page로 반환하도록 변경

--- src/main/java/com/san/springbd/controller/HomeController.java
+++ src/main/java/com/san/springbd/controller/HomeController.java
@@ -3,28 +3,28 @@ package com.san.springbd.controller;
 import com.san.springbd.domain.Board;
 import com.san.springbd.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
-@Slf4j
 @RequiredArgsConstructor
 public class HomeController {
 
     private final BoardService boardService;
 
     @RequestMapping("/")
-    public String home(Model model){
-       log.info("home controller");
-
-       List<Board> boards = boardService.findBoards();
+    public String home(@RequestParam(name = "page",defaultValue = "0") int page, Model model){
+        PageRequest pageRequest = PageRequest.of(page,5,Sort.by("createDate").descending());
+       Page<Board> boards = boardService.findBoards(pageRequest);
 
        model.addAttribute("boards",boards);
-
+       model.addAttribute("maxPage",5);
        return "home";
     }
 }
