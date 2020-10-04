0ecbf0457c37e5763913038b7011eda297dce732
ksan41 <aquam41@gmail.com>
2020-10-04 18:16:35 +0900

    Update HomeController.java
    게시글 검색 기능도 같이 처리하도록 변경.

--- src/main/java/com/san/springbd/controller/HomeController.java
+++ src/main/java/com/san/springbd/controller/HomeController.java
@@ -1,11 +1,11 @@
 package com.san.springbd.controller;
 
 import com.san.springbd.domain.Board;
+import com.san.springbd.domain.BoardSearch;
 import com.san.springbd.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +19,23 @@ public class HomeController {
     private final BoardService boardService;
 
     @RequestMapping("/")
-    public String home(@RequestParam(name = "page",defaultValue = "0") int page, Model model){
-        PageRequest pageRequest = PageRequest.of(page,5,Sort.by("createDate").descending());
-       Page<Board> boards = boardService.findBoards(pageRequest);
+    public String home(@RequestParam(name = "page",defaultValue = "0") int page, Model model,
+                       @RequestParam(name="boardSearch",defaultValue = "") String boardSearch,
+                       String keyword){
+        BoardSearch rqBoardSearch;
+        switch (boardSearch){
+            case "nickname": rqBoardSearch = BoardSearch.NICKNAME;break;
+            case "title" : rqBoardSearch = BoardSearch.TITLE;break;
+            case "content" : rqBoardSearch = BoardSearch.CONTENT;break;
+            default:rqBoardSearch = null;break;
+        }
+        PageRequest pageRequest = PageRequest.of(page,5);
+       Page<Board> boards = boardService.findBoards(pageRequest,rqBoardSearch,keyword);
 
        model.addAttribute("boards",boards);
        model.addAttribute("maxPage",5);
+       model.addAttribute("boardSearch",boardSearch);
+       model.addAttribute("keyword",keyword);
        return "home";
     }
 }
