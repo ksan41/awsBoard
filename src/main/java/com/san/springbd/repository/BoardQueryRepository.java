3d107292848b5df3cb792936a73768e8e5210dcd
ksan41 <aquam41@gmail.com>
2020-10-04 18:15:03 +0900

    Update BoardQueryRepository.java
    기존 게시글 전체조회 메서드(findAll)을
    검색 조건 값이 있을 경우 검색 결과를 리턴하도록 개선

--- src/main/java/com/san/springbd/repository/BoardQueryRepository.java
+++ src/main/java/com/san/springbd/repository/BoardQueryRepository.java
@@ -1,8 +1,10 @@
 package com.san.springbd.repository;
 
-import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.san.springbd.domain.Board;
+import com.san.springbd.domain.BoardSearch;
 import com.san.springbd.domain.BoardStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,14 +20,30 @@ import static com.san.springbd.domain.QBoard.board;
 public class BoardQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public Page<Board> findAll(PageRequest pageRequest){
-        QueryResults<Board> result =
-                 queryFactory.selectFrom(board)
-                .where(board.status.eq(BoardStatus.VISIBLE))
-                .orderBy(board.createDate.desc())
+    public Page<Board> findAll(PageRequest pageRequest, BoardSearch boardSearch,String keyword){
+        JPQLQuery query = queryFactory.selectFrom(board)
+                          .where(board.status.eq(BoardStatus.VISIBLE),
+                                  nicknameEq(boardSearch,keyword),
+                                  titleEq(boardSearch,keyword),
+                                  contentEq(boardSearch,keyword));
+
+        query.orderBy(board.createDate.desc())
                 .offset(pageRequest.getOffset())
-                .limit(pageRequest.getPageSize())
-                .fetchResults();
-        return new PageImpl<>(result.getResults(),pageRequest,result.getTotal());
+                .limit(pageRequest.getPageSize());
+
+        return new PageImpl<>(query.fetch(),pageRequest,query.fetchCount());
+    }
+
+    private BooleanExpression nicknameEq(BoardSearch boardSearch,String keyword){
+        return boardSearch == BoardSearch.NICKNAME ? board.nickname.like("%"+keyword+"%") : null;
+    }
+
+    private BooleanExpression titleEq(BoardSearch boardSearch,String keyword){
+        return boardSearch == BoardSearch.TITLE ? board.title.like("%"+keyword+"%") : null;
+    }
+
+    private BooleanExpression contentEq(BoardSearch boardSearch,String keyword){
+        return boardSearch == BoardSearch.CONTENT ?
+                board.title.like("%"+keyword+"%").or(board.content.like("%"+keyword+"%")) : null;
     }
 }
